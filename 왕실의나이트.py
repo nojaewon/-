660109fdc186bb4937f02aa551b501993bbf3d17
NoJe <https://github.com/harryyim707/dev-api-akoletter.git>
2023-05-27 13:49:50 +0900

    왕실의나이트

---
+++ 왕실의나이트.py
@@ -0,0 +1,14 @@
+# 8 x 8 좌표 평면상에서 나이트의 위치가 주어졌을 때 이동할 수 있는 경우의 수
+
+pos = input()
+move = [(-2, -1), (-1, -2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, 2), (-2, 1), (-2, -1), (-1, -2)]
+row = ord(pos[0]) - ord('a')
+col = int(pos[1])
+count = 0
+
+for k in move:
+    if (0 <= row + k[0] and row + k[0] < 8) and (0 <= col + k[1] and col + k[1] < 8):
+        count += 1
+
+print(count)
+
