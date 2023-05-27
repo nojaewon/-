397c7f3caeb425a39fc6e9f34ff51de5e9ee5b50
NoJe <https://github.com/harryyim707/dev-api-akoletter.git>
2023-05-27 12:53:22 +0900

    숫자카드게임

---
+++ 숫자카드게임.py
@@ -0,0 +1,20 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().rstrip().split())
+arr = []
+
+for i in range(n):
+    arr.append(list(map(int ,sys.stdin.readline().rstrip().split())))
+
+max_value = 0
+
+for i in range(n):
+    min_value = 10000
+    for j in range(m):
+        if min_value > arr[i][j]:
+            min_value = arr[i][j]
+    
+    if max_value < min_value:
+        max_value = min_value
+
+print(max_value)
